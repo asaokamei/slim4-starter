e0a488613ca0f84e0d16813d849697535048a5a8
亀井亜佐夫 <asao.kamei@gmail.com>
2021-01-04 15:49:29 +0900

    add logging to AppMiddleware and ErrorHandler.

--- app/Application/Middleware/AppMiddleware.php
+++ app/Application/Middleware/AppMiddleware.php
@@ -4,10 +4,12 @@
 namespace App\Application\Middleware;
 
 
+use App\Application\Container\Current;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Log\LoggerInterface;
 use Slim\App;
 
 class AppMiddleware implements Middleware
@@ -18,10 +20,15 @@ class AppMiddleware implements Middleware
      * @var App
      */
     private $app;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(App $app)
+    public function __construct(App $app, LoggerInterface $logger)
     {
         $this->app = $app;
+        $this->logger = $logger;
     }
 
     /**
@@ -29,6 +36,12 @@ class AppMiddleware implements Middleware
      */
     public function process(Request $request, RequestHandler $handler): Response
     {
+        if ($this->logger) {
+            $url = $request->getUri()->__toString();
+            $method = $request->getMethod();
+            $this->logger->info("{$method} {$url}");
+        }
+
         $request = $request->withAttribute(self::APP_NAME, $this->app);
 
         return $handler->handle($request);
